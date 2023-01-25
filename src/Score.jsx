0d50e2c2f451dbfc258f9e472904dba7f411808e
KevinRoost <kroost92@gmail.com>
2023-01-24 21:36:03 -0500

    basic styling

--- src/Score.jsx
+++ src/Score.jsx
@@ -1,11 +1,9 @@
-import Moment from 'react-moment'
-
 const Score = (props) => {
   let dateSplit = props.score.date.split("-")
 
   return (
     <>
-      <p>{props.score.score}% on {dateSplit[1]}/{dateSplit[2]}</p>
+      <li>{props.score.score}% on {dateSplit[1]}/{dateSplit[2]}</li>
     </>
   )
 }
--- src/Student.jsx
+++ src/Student.jsx
@@ -4,10 +4,12 @@ const Student = (props) => {
   return (
     <>
       <div className="card">
-        <h1 className="name">{props.student.name}</h1>
-        <p className="bio">{props.student.bio}</p>
-        <h3>{props.student.name.split(" ")[0]} earned the following scores:</h3>
+        <div>
+          <h1 className="name">{props.student.name}</h1>
+          <p className="bio">{props.student.bio}</p>
+        </div>
         <ul>
+          <h3>{props.student.name.split(" ")[0]} earned the following scores:</h3>
           {props.student.scores.map((score, idx) => 
             <Score 
               key={idx} 
